869c286bde0695b78a816d79a6297f5298de7c45
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-07-10 11:36:39 -0600

    Update script.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -33,10 +33,13 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled");
-    } else {
-      $("#mainNav").removeClass("navbar-scrolled");
+    var nav=$("#mainNav");
+    if(nav.length){
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-scrolled");
+      } else {
+        $("#mainNav").removeClass("navbar-scrolled");
+      }
     }
   };
   // Collapse now if page is not at top
