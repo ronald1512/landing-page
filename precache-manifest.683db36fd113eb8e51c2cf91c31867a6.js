1232f65caa6ea4335d687457f38f59d74b16560a
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-07-10 10:51:45 -0600

    Updates

---
+++ precache-manifest.683db36fd113eb8e51c2cf91c31867a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df6f6aa59a79d7b2924496373da2cdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "541221e92df787a5b26d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82358a3d9d9425e95fc1",
+    "url": "/static/js/2.527bda6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.527bda6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541221e92df787a5b26d",
+    "url": "/static/js/main.cddf6667.chunk.js"
+  },
+  {
+    "revision": "a51bdce1b2dc413c1162",
+    "url": "/static/js/runtime-main.b34440fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
