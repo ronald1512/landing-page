c1e5fb60a61d11a937624507a39be6cdab5449f2
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-07-12 21:01:18 -0600

    Update Services and Contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-
+//                     <i className="fas fa-phone fa-3x mb-3 text-muted" />
 class Contact extends Component{
     render(){
         return (
@@ -11,12 +11,12 @@ class Contact extends Component{
                   <div className="col-lg-8 text-center">
                     <h2 className="mt-0">Contáctenos!</h2>
                     <hr className="divider my-4" />
-                    <p className="text-muted mb-5">¿Listo para cotizar con nosotros? ¡Llámanos o envíanos un email y nos comunicaremos contigo tan pronto como sea posible!</p>
+                    <p className="text-muted mb-5">¿Listo para cotizar con nosotros? ¡Llámanos, mándanos un mensaje o envíanos un email y nos comunicaremos contigo tan pronto como sea posible!</p>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
-                    <i className="fas fa-phone fa-3x mb-3 text-muted" />
+                    <i className="fab fa-whatsapp fa-3x mb-3 text-muted" />
                     <div>+502 3015-0785</div>
                   </div>
                   <div className="col-lg-4 mr-auto text-center">
--- src/Components/Services.js
+++ src/Components/Services.js
@@ -12,28 +12,28 @@ class Services extends Component {
                     <div className="row">
                         <div className="col-lg-3 col-md-6 text-center">
                             <div className="mt-5">
-                                <i className="fas fa-4x fa-gem text-primary mb-4" />
+                                <i className="fas fa-4x fa-head-side-mask text-primary mb-4" />
                                 <h3 className="h4 mb-2">Pilotos Profesionales</h3>
                                 <p className="text-muted mb-0">Todos nuestros pilotos son experimentados y portan protector facial, mascarilla, guantes y uniforme completo. </p>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 text-center">
                             <div className="mt-5">
-                                <i className="fas fa-4x fa-laptop-code text-primary mb-4" />
+                                <i className="fas fa-4x fas fa-people-arrows text-primary mb-4" />
                                 <h3 className="h4 mb-2">Sin aglomeraciones</h3>
                                 <p className="text-muted mb-0">Las unidades transitan con el 50% de su capacidad, según su tarjeta de circulación. </p>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 text-center">
                             <div className="mt-5">
-                                <i className="fas fa-4x fa-globe text-primary mb-4" />
+                                <i className="fas fa-4x fa-hands-wash text-primary mb-4" />
                                 <h3 className="h4 mb-2">Cuidamos tu camino</h3>
                                 <p className="text-muted mb-0">Dispensadores de alcohol en gel para el ingreso y egreso. Todas las unidades cuentan con seguro de pasajeros.</p>
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 text-center">
                             <div className="mt-5">
-                                <i className="fas fa-4x fa-heart text-primary mb-4" />
+                                <i className="fas fa-4x fa-hand-sparkles text-primary mb-4" />
                                 <h3 className="h4 mb-2">Limpieza constante</h3>
                                 <p className="text-muted mb-0">Las unidades son sanitizadas por dentro y por fuera antes del abordaje de pasajeros. </p>
                             </div>
